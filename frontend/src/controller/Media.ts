fe8c83ed759a3c6e65a6ec6482cd33de1558c63b
poseydurling <margaret_durling@brown.edu>
2022-11-27 18:27:23 -0500

    pushing media draft

--- frontend/src/controller/Media.ts
+++ frontend/src/controller/Media.ts
@@ -1,7 +1,8 @@
-import rl_data  from "./mockedData/fullDownload.json"
+import { myKey } from '../private/key';
+import { FeatureCollection } from "geojson";
 
-export function getMovieData(): Promise<GeoJSON.FeatureCollection | undefined> {
-    const dataResponse: Promise<GeoJSON.FeatureCollection | undefined> = fetch(`http://localhost:3232/getMap?minLat=${minLat}&minLon=${minLon}&maxLat=${maxLat}&maxLon=${maxLon}`)
+function getMovieData(): Promise<GeoJSON.FeatureCollection | undefined> {
+    const dataResponse: Promise<GeoJSON.FeatureCollection | undefined> = fetch(`https://api.themoviedb.org/3/movie/550?api_key=${myKey}`)
     .then((resp) => resp.json())
     .then((json) => {
         if (isFeatureCollection(json.data)) {
@@ -11,27 +12,11 @@ export function getMovieData(): Promise<GeoJSON.FeatureCollection | undefined> {
             console.log("Undefined data")
             return undefined
           }})
-
     return dataResponse;
 }
 
 export function isFeatureCollection(json: any): json is FeatureCollection {
-    return json.type === "FeatureCollection"
+  return json.type === "FeatureCollection"
 }
-
-function getHardCodedData(): GeoJSON.FeatureCollection | undefined{
-    if (isFeatureCollection(rl_data)) {
-      return rl_data
-    }
-    return undefined
-  }
-  
-  export function getMockData(): Promise<GeoJSON.FeatureCollection | undefined> {
-    if (isFeatureCollection(rl_data)) {
-      return new Promise((resolve) => resolve(getHardCodedData()))
-    }
-    return new Promise((resolve) => resolve(undefined))
-  
-  }
 
 export {getMovieData}
---
+++ frontend/src/private/key.ts
@@ -0,0 +1,3 @@
+let myKey: string = "pk.eyJ1Ijoic2FtLXNodWxtYW4iLCJhIjoiY2xhNXFhZjB6MHNuczNzcHFlejk4NjNmbyJ9.0cbQs36Hzw5DOK1MmQH8ag";
+
+export {myKey}
