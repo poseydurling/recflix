e20045b2d4af8e42b72dc20176e696c928e1f907
poseydurling <margaret_durling@brown.edu>
2022-11-27 17:40:59 -0500

    set up posey's user stories but not done

--- frontend/src/controller/Media.ts
+++ frontend/src/controller/Media.ts
@@ -1,1 +1,37 @@
-export {}
+import rl_data  from "./mockedData/fullDownload.json"
+
+export function getMovieData(): Promise<GeoJSON.FeatureCollection | undefined> {
+    const dataResponse: Promise<GeoJSON.FeatureCollection | undefined> = fetch(`http://localhost:3232/getMap?minLat=${minLat}&minLon=${minLon}&maxLat=${maxLat}&maxLon=${maxLon}`)
+    .then((resp) => resp.json())
+    .then((json) => {
+        if (isFeatureCollection(json.data)) {
+            console.log(json.data); 
+            return json.data
+          } else {
+            console.log("Undefined data")
+            return undefined
+          }})
+
+    return dataResponse;
+}
+
+export function isFeatureCollection(json: any): json is FeatureCollection {
+    return json.type === "FeatureCollection"
+}
+
+function getHardCodedData(): GeoJSON.FeatureCollection | undefined{
+    if (isFeatureCollection(rl_data)) {
+      return rl_data
+    }
+    return undefined
+  }
+  
+  export function getMockData(): Promise<GeoJSON.FeatureCollection | undefined> {
+    if (isFeatureCollection(rl_data)) {
+      return new Promise((resolve) => resolve(getHardCodedData()))
+    }
+    return new Promise((resolve) => resolve(undefined))
+  
+  }
+
+export {getMovieData}
