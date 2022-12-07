efb7d9269f367571918150abc1833efa333f8e5f
Cedric Sirianni <cds1@sirianni.org>
2022-12-06 22:28:40 -0500

    add error docstrings

--- backend/app.py
+++ backend/app.py
@@ -9,10 +9,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 class ResponseError(Exception):
+    """Response exception base class"""
     pass
 
 
 class BadRequestError(ResponseError):
+    """Custom exception class for when the client sends an invalid request"""
     code = 400
     description = ("the request was missing a needed field, or the field was"
                    " ill-formed")
