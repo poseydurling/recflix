add7431109255fc175ba99adc358f06a6fc8e957
Cedric Sirianni <cds1@sirianni.org>
2022-12-08 00:34:47 -0500

    improve app.py styling with black formatter

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,29 @@
-from flask import Flask, jsonify, request
 import logging
+from flask import Flask, jsonify, request
 
-app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
+app = Flask(__name__)
+
+
 # errors
-
-
 class ResponseError(Exception):
     """Response exception base class"""
+
     pass
 
 
 class BadRequestError(ResponseError):
     """Custom exception class for when the client sends an invalid request"""
+
     code = 400
-    description = ("the request was missing a needed field, or the field was"
-                   " ill-formed")
+    description = (
+        "the request was missing a needed field, or the field was" " ill-formed"
+    )
 
 
 # handlers
-
-
 @app.errorhandler(ResponseError)
 def handle_exception(err):
     """Returns a JSON when ResponseError or its children are raised"""
@@ -30,7 +31,7 @@ def handle_exception(err):
     return jsonify(response), err.code
 
 
-@app.route("/recommendations/", methods=['POST'])
+@app.route("/recommendations/", methods=["POST"])
 def get_recommendations():
     """
     Returns a JSON  containing a list of movie recommendations given three
@@ -46,25 +47,26 @@ def get_recommendations():
     app.logger.info(f"Example 1: {example1}")
     app.logger.info(f"Example 2: {example2}")
     app.logger.info(f"Example 3: {example3}")
-    return jsonify({"recommendation_ids":
-                    [671, 5, 11, 12, 13, 14, 16, 18, 19, 20]})
+    return jsonify({"recommendation_ids": [671, 5, 11, 12, 13, 14, 16, 18, 19, 20]})
 
 
-@app.route("/titles_to_ids/", methods=['GET'])
+@app.route("/titles_to_ids/", methods=["GET"])
 def get_titles_to_ids():
     """Returns a JSON containing every movie title and its corresponding id"""
-    return jsonify({
-        "Avatar": 19995,
-        "Pirates of the Caribbean: At World's End": 285,
-        "Spectre": 206647,
-        "The Dark Knight Rises": 49026,
-        "John Carter": 49529,
-        "Spider-Man 3": 559,
-        "Tangled": 38757,
-        "Avengers: Age of Ultron": 99861,
-        "Harry Potter and the Half-Blood Prince": 767,
-        "Batman v Superman: Dawn of Justice": 209112
-        })
+    return jsonify(
+        {
+            "Avatar": 19995,
+            "Pirates of the Caribbean: At World's End": 285,
+            "Spectre": 206647,
+            "The Dark Knight Rises": 49026,
+            "John Carter": 49529,
+            "Spider-Man 3": 559,
+            "Tangled": 38757,
+            "Avengers: Age of Ultron": 99861,
+            "Harry Potter and the Half-Blood Prince": 767,
+            "Batman v Superman: Dawn of Justice": 209112,
+        }
+    )
 
 
 if __name__ == "__main__":
