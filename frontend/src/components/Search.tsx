5ed6ff75673cb5c8363d964017e65684ed07ef3a
faishim <fshimye@gmail.com>
2022-12-12 22:59:42 -0500

    finished all fetches and linking to respective buttons

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,6 +1,6 @@
-import {getAllMovies, getAutocompleteList} from "../controller/Search"
+import {getAllMovies, searchTitle} from "../controller/Search"
 import {useState} from "react";
-// import { AutoComplete } from 'rsuite'
+import { AutoComplete } from 'rsuite'
 // import 'rsuite/dist/rsuite.min.css';
 import loadMovieImage from "../controller/Recommendation"
 
@@ -11,26 +11,28 @@ const options = ['Monday', 'Tuesday']
 
 export default function Search(){
     const [search, setSearch] = useState('');
-    const availableTitles = () => {getAutocompleteList()};
     const handleChange = (val: any) => {
         setSearch(val);
       }; 
     return (    
         <div>
             {/* onChange={(event) => {setSearch(event.target.value)}} */} 
-           {/* <AutoComplete style={{width: '100%'}} id="search-box" placeholder="Enter a movie title here!" data={availableTitles} value={search} onChange={handleChange}/>
-           */}
-            <button type="submit" id="submit1" onClick={() => {getAutocompleteList()}}>Search</button>
+           <AutoComplete style={{width: '100%'}} id="search-box" placeholder="Enter a movie title here!" data={options} value={search} onChange={handleChange}/>
+            <button type="submit" id="submit1" onClick={() => {buildRecommendationsbySearch(search)/*getAutocompleteList()*/}}>Search</button>
         </div>
     )
 }
+export let recMovlist: any[] = []
 
-// function registerAutocomplete(search: String) {
-//       var availableTitles = () => {getMovieAutocomplete(search)};
-//     //   $( "#search-box" ).autocomplete({
-//     //     source: availableTitles
+function buildRecommendationsbySearch(search: string) {
 
-//     //   });
-
-//     getAllMovies()
-// }
+    //build the movie ID lists recommended from the 3 searched titles
+    //it'll be used when clicking the recommendation button
+    searchTitle(search).then(reclist =>{
+        if(reclist !== null) {
+            for(let i=0; i<reclist.data.length; i++) {
+                recMovlist[i] = reclist.data[i]
+            }
+        }
+    })
+}
--- frontend/src/controller/Search.tsx
+++ frontend/src/controller/Search.tsx
@@ -1,10 +1,5 @@
 import { sendPost } from "../components/RecommendButton";
 
-export let data = [];
-let finalDataStructure = new Map()
-let keys  = new Array() 
-keys.push(finalDataStructure.keys())
-//cedric sprint 3
 interface DB {
   [name: string]: number;
 }
@@ -12,86 +7,43 @@ interface DB {
 export async function getAllMovies(){
   const response = await fetch('http://127.0.0.1:5000/titles_to_ids/')
   const moviedata = await response.json()
+
   return moviedata.data;
 }
 
-// export async function getMovieMap(){
-//   getAllMovies().then(m_data =>{
-    
-//     let movieHashmap = new Map();
-//     for (var name in m_data.data) {
-//         movieHashmap.set(name, m_data.data[name]);
-//     }
-//     console.log(movieHashmap);
-//     return movieHashmap;
-//   }
-//   )
-// }
+let movId = [0,0,0]; //three user inputted movie IDs
+let movNum = 0; //movId index
+let movCnt = 0; //counts for number of searched movies
 var datadb: (DB|null) = null;
 
-//gets id of the movie
 export async function searchTitle(name:string){
-  if(datadb === null){
-    let datadb = await deserialize() as DB
-    const id = datadb[name]
-    console.log(datadb)
-    await sendPost(id)
-    return datadb[name] 
+  if(name === ""){
+    alert("No search input")
+    return null;
+  } else {
+    if(datadb === null) {
+      datadb = await getAllMovies() as DB
+    }
+
+    if(datadb[name] == null) {
+      alert("No match exist!")
+      return null;
+    } else {
+      const id = datadb[name]
+      movId[movNum] = id
+      if(movNum == 2){
+        movNum = 0
+      } else {
+        movNum++
+      }
+
+      if(movCnt > 1) {
+        const rec_ret = await sendPost(movId)
+        return rec_ret
+      } else {
+        movCnt++
+        return null
+      }
+    }
   }
-}
-
-export async function deserialize(){
-  if(datadb === null) {
-    return datadb = await getAllMovies() as DB
-  }
-}
-
-export function getAutocompleteList(){
-  deserialize();
-  if(datadb != null) {
-    console.log(datadb.name)
-    //map.key
-    return datadb.name.toString();
-  }
-}
-
-export async function serializeMovies(){
-  getAllMovies().then(data =>{
-    //this method should return some sort of data structure with all movie titles
-    //linked to movie IDs
-    
-    //update finalDataStructure
-    return finalDataStructure
-  }
-  )
-}
-
-// export async function searchTitle(name:String){
-//   serializeMovies().then(data =>{
-//     //return output
-//   })
-
-//   for(let i =0; i<keys.length; i++){
-//     if (keys[i] == name){
-//       return finalDataStructure.get(i)
-//     }
-//     else{
-//       i++
-//     }
-//   }
-  
-// }
-
-export function usersMovies(){
-  //create a list of movie IDs wnated by users
-}
-
-export function getMovieAutocomplete(name:String){
-  //change this local host once backend is done
-  console.log(getAllMovies)
-  // await fetch(`http://localhost:3232/getMap?minLat=${name}`)
-  // .then(response => response.json())
-  // .then(json => {
-  //   data = json["title"]
-  // })
 }
