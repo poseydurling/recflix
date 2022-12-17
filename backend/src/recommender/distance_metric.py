305ae4070229ff5a02b8e3a9eb6227cd6104d291
Cedric Sirianni <cds1@sirianni.org>
2022-12-17 14:40:49 -0500

    add docstrings to distance metric functions

--- backend/src/recommender/distance_metric.py
+++ backend/src/recommender/distance_metric.py
@@ -1,17 +1,42 @@
 from sklearn.metrics.pairwise import pairwise_distances
 
-
+# pairwise distances reference: https://scikit-learn.org/0.15/modules/generated/sklearn.metrics.pairwise.pairwise_distances.html
 def distance_metric(X, Y, metric):
+    """A pseudo-interface for the below distance metric functions.
+
+    :param X: an array of ints
+    :param Y: an array of ints
+    :return: a distance matrix
+    """
     pass
 
 
 def cosine_distance(X, Y):
+    """Compute the cosine distance matrix from vector arrays X and Y.
+
+    :param X: an array of ints
+    :param Y: an array of ints
+    :return: a cosine distance matrix
+    """
     return pairwise_distances(X, Y, metric="cosine")
 
 
 def euclidean_distance(X, Y):
+    """Compute the euclidean distance matrix from vector arrays X and Y.
+
+    :param X: an array of ints
+    :param Y: an array of ints
+    :return: a euclidean distance matrix
+    """
     return pairwise_distances(X, Y, metric="euclidean")
 
 
 def correlation_distance(X, Y):
+    """Compute the (Pearson) correlation distance matrix from vector arrays X
+    and Y.
+
+    :param X: an array of ints
+    :param Y: an array of ints
+    :return: a correlation distance matrix
+    """
     return pairwise_distances(X, Y, metric="correlation")
