41c7d398f4dc1f999f7354f6a14ee5d06c27d6a7
angie <angela_osei-ampadu@brown.edu>
2022-12-18 13:18:19 -0500

    updated git ignore to ignore the private key

--- frontend/private/key.tsx
+++
@@ -1,4 +0,0 @@
-// let myKey: string = "pk.eyJ1Ijoic2FtLXNodWxtYW4iLCJhIjoiY2xhNXFhZjB6MHNuczNzcHFlejk4NjNmbyJ9.0cbQs36Hzw5DOK1MmQH8ag";
-let myKey: string = "9b27799525fd01f81d33f56b5b0af5fb"
-export {myKey}
-
