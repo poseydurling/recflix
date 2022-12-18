7b7a6eae9305babf4458e19c7516373f71392179
poseydurling <margaret_durling@brown.edu>
2022-12-18 17:05:11 -0500

    cleanup frontend folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,14 @@
-import { useState } from 'react';
-import { BrowserRouter, Route, Routes, Router, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css'
 import Form from './views/Form';
-
 import Recommendation from './views/Recommendation';
-import { fetchPoster } from './controller/Media';
-import {movieID} from './controller/Search';
-// import NewRecommendation from './views/NewRecommendation'
-
-
-
 
 function App () {
-  const [search, setSearch] = useState('');
-  const handleChange = (val: any) => {
-    setSearch(search);
-  }
-
-  const [card1, setcard1] = useState('');
-  const [card2, setcard2] = useState('');
-  const [card3, setcard3] = useState('');
-
-  // const handleSettingCard = async()=>{
-  //   const posterResponse1 = await fetchPoster(search);
-  //   // setcard1(posterResponse1)
-
-  //   const posterResponse2 = await fetchPoster(search);
-  //   // setcard2(posterResponse2)
-
-  //   const posterResponse3 = await fetchPoster(search);
-  //   // setcard3(posterResponse3)
-  // }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<Form card1={card1} card2={card2} card3={card3}  search={search} setSearch={setSearch}/>} />
+        <Route path = "/" element={<Form/>} />
         <Route path="/recommendationPage" element={<Recommendation/>} />
-        {/* <Route path="/newRecommendationPage" element={<NewRecommendation/>} /> */}
-     <Route path = "/" element={<Form card1={card1} card2={card2} card3={card3}  search={search} setSearch={setSearch}/>} />
       </Routes>
     </BrowserRouter>
     
--- frontend/src/components/ReturnHomeButton.tsx
+++ frontend/src/components/ReturnHomeButton.tsx
@@ -1,19 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import setCount from "./TryAgainButton"
+
 export default function NewRequestButton() {
     const navigateToRecPage = useNavigate();
-
-    const [search, setSearch] = useState('');
     return (
         <div>
             <button type="submit" id="newRequestButton"
                 onClick={() => { navigateToRecPage('/')
                 setCount()
-    }
-            
-    }
-            /*onClick={() => {getMovieAutocomplete(search)}}*/>Go Back to Home</button>
+                }
+            }>Go Back to Home</button>
 
         </div>
     )
