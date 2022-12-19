bff771d614f08426199d3fea4853df1508585512
faishim <fshimye@gmail.com>
2022-12-19 00:30:04 -0500

    testing for fetches

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { myKey } from './private/key';
-import { getRecommendation } from './controller/Search';
+import { getAllMovies, getRecommendation } from './controller/Search';
+import { ResultType } from '@remix-run/router/dist/utils';
 
 const expectedOutput = [175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771]
 
@@ -12,7 +13,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('fetches to backend for master movie list', async () => {
+test('testing fetch for getAllMovies and getMovieTitleList', async () => {
   const url = 'http://127.0.0.1:5000/titles_to_ids/'
   const result = await fetch(url)
     .then(response => response.json())
@@ -22,7 +23,7 @@ test('fetches to backend for master movie list', async () => {
   expect(result['data']).toBeValid
 })
 
-test('fetches to backend for recommended movies', async () => {
+test('testing fetch for sendPost', async () => {
   const result = await fetch('http://127.0.0.1:5000/recommendations/', {
     method: 'POST',
     headers: {
@@ -36,7 +37,7 @@ test('fetches to backend for recommended movies', async () => {
   expect(response.data).toStrictEqual([175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771])
 })
 
-test('fetches to api for recommended movie information', async () => {
+test('testing fetch for getMovieDetails, getMovieInformation, and fetchPoster', async () => {
   const url = 'https://api.themoviedb.org/3/movie/175574?api_key='+myKey+'&language=en-US'
   const result = await fetch(url)
     .then(response => response.json())
@@ -48,9 +49,11 @@ test('fetches to api for recommended movie information', async () => {
 })
 
 test('search controller getRecommendation function', async () =>{
-  getRecommendation("A Christmas Carol")
-  getRecommendation("A Christmas Story")
-  //getRecommendation("A Charlie Brown Christmas")
+  await getRecommendation("A Christmas Carol"); 
+  await getRecommendation("A Christmas Story");
+  const result = await getRecommendation("A Charlie Brown Christmas")
 
-  //expect(getRecommendation("A Charlie Brown Christmas")).toReturn(Array)
+  const response = await result
+
+  expect(response).toStrictEqual({"data": [175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771], "status": "success"})
 })
