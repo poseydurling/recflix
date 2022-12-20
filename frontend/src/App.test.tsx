94b9c08ea2528fc3d08292b6528986b8bfff6164
faishim <fshimye@gmail.com>
2022-12-20 11:54:48 -0500

    uncommented tests for fetches

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -13,8 +13,6 @@ import userEvent from '@testing-library/user-event';
 import { execPath } from 'process';
 import exp from 'constants';
 
-
-
 beforeEach(() => {
   render(<App />);
 });
@@ -23,56 +21,56 @@ beforeEach(() => {
   render(<Recommendation />);
 });
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-// test('testing fetch for getAllMovies and getMovieTitleList', async () => {
-//   const url = 'http://127.0.0.1:5000/titles_to_ids/'
-//   const result = await fetch(url)
-//     .then(response => response.json())
-//     .then(data => data)
+test('testing fetch for getAllMovies and getMovieTitleList', async () => {
+  const url = 'http://127.0.0.1:5000/titles_to_ids/'
+  const result = await fetch(url)
+    .then(response => response.json())
+    .then(data => data)
 
-//   expect(result['status']).toBe('success')
-//   expect(result['data']).toBeValid
-// })
+  expect(result['status']).toBe('success')
+  expect(result['data']).toBeValid
+})
 
-// test('testing fetch for sendPost', async () => {
-//   const result = await fetch('http://127.0.0.1:5000/recommendations/', {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ "example1": 13187, "example2" : 17979, "example3": 850})
-// })
-//   const response = await result.json()
-//   expect(result['status']).toBe(200)
-//   expect(response.data).toStrictEqual([175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771])
-// })
+test('testing fetch for sendPost', async () => {
+  const result = await fetch('http://127.0.0.1:5000/recommendations/', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "example1": 13187, "example2" : 17979, "example3": 850})
+})
+  const response = await result.json()
+  expect(result['status']).toBe(200)
+  expect(response.data).toStrictEqual([175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771])
+})
 
-// test('testing fetch for getMovieDetails, getMovieInformation, and fetchPoster', async () => {
-//   const url = 'https://api.themoviedb.org/3/movie/175574?api_key='+myKey+'&language=en-US'
-//   const result = await fetch(url)
-//     .then(response => response.json())
-//     .then(data => data)
+test('testing fetch for getMovieDetails, getMovieInformation, and fetchPoster', async () => {
+  const url = 'https://api.themoviedb.org/3/movie/175574?api_key='+myKey+'&language=en-US'
+  const result = await fetch(url)
+    .then(response => response.json())
+    .then(data => data)
 
-//   expect(result['adult']).toBe(false)
-//   expect(result['original_title']).toBe("Free Birds")
-//   expect(result['poster_path']).toBe("/gnSU2wUBq2gTkBEkxY8C1d1fXAQ.jpg")
-// })
+  expect(result['adult']).toBe(false)
+  expect(result['original_title']).toBe("Free Birds")
+  expect(result['poster_path']).toBe("/gnSU2wUBq2gTkBEkxY8C1d1fXAQ.jpg")
+})
 
-// test('search controller getRecommendation function', async () =>{
-//   await getRecommendation("A Christmas Carol"); 
-//   await getRecommendation("A Christmas Story");
-//   const result = await getRecommendation("A Charlie Brown Christmas")
+test('search controller getRecommendation function', async () =>{
+  await getRecommendation("A Christmas Carol"); 
+  await getRecommendation("A Christmas Story");
+  const result = await getRecommendation("A Charlie Brown Christmas")
 
-//   const response = await result
+  const response = await result
 
-//   expect(response).toStrictEqual({"data": [175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771], "status": "success"})
-// })
+  expect(response).toStrictEqual({"data": [175574, 13673, 18147, 8871, 51052, 9279, 9969, 1268, 9043, 771], "status": "success"})
+})
 
 
 test('checking components from the Form page', () => {
